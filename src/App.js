d55165e9da5af9407dcad2e64c86ad09d913132d
jiwind-two <75973495+jiwind-two@users.noreply.github.com>
2022-05-04 19:26:52 +0900

    atomicd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
+      <br />
+      <br />
       <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <br />
+      <SearchInput />
     </div>
   );
 }
---
+++ src/components/molecules/SearchInput.jsx
@@ -0,0 +1,10 @@
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { Input } from "../atoms/Input/Input";
+export const SearchInput = () => {
+  return (
+    <div>
+      <Input placeholder="検索条件を入力" />
+      <PrimaryButton>検索</PrimaryButton>
+    </div>
+  );
+};

0e67f8be6b1eed29d8b57c204c90dbcb2ba70287
jiwind-two <75973495+jiwind-two@users.noreply.github.com>
2022-05-05 17:37:23 +0900

    atomicd2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
 import "./styles.css";
 
 const user = {
@@ -19,12 +20,12 @@ const user = {
 
 export default function App() {
   return (
-    <div>
+    <HeaderOnly>
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
       <UserCard user={user} />
-    </div>
+    </HeaderOnly>
   );
 }

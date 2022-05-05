cfa37dfef3ecc029e7e12638994ca5f6002f039f
jiwind-two <75973495+jiwind-two@users.noreply.github.com>
2022-05-05 22:41:56 +0900

    atomicd2

--- src/components/templates/DefaultLayout.jsx
+++ src/components/templates/DefaultLayout.jsx
@@ -1,11 +1,13 @@
+import { Footer } from "../atoms/layout/Footer";
 import { Header } from "../atoms/layout/Header";
 
-export const HeaderOnly = (props) => {
+export const DefaultLayout = (props) => {
   const { children } = props;
   return (
     <>
       <Header />
       {children}
+      <Footer />
     </>
   );
 };
